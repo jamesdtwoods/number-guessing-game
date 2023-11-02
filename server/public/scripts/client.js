ab45f4261bb3816ed38c4aeaff725f133bb56a6e
James Woods <james.d.t.woods@gmail.com>
2023-11-02 15:01:42 -0500

    restarting with a blank slate. working through steps 1-3 on the whiteboard plan

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,171 @@ function onReady() {
 }
 
 onReady()
+
+
+function onReset() {
+  axios({
+    method: 'POST',
+    url: '/randomNumber'
+  })
+}
+
+
+function submitRandom(event) {
+  event.preventDefault()
+  let playerOne = document.getElementById('playerOne').value;
+  let playerTwo = document.getElementById('playerTwo').value;
+  let playerThree = document.getElementById('playerThree').value;
+  let numberOne = document.getElementById('oneNumber').value;
+  let numberTwo = document.getElementById('twoNumber').value;
+  let numberThree = document.getElementById('threeNumber').value;
+  document.getElementById('playerOne').value = '';
+  document.getElementById('playerTwo').value = '';
+  document.getElementById('playerThree').value = '';
+  document.getElementById('oneNumber').value = '';
+  document.getElementById('twoNumber').value = '';
+  document.getElementById('threeNumber').value = '';
+  let newRound = [
+    {
+      playerOne: playerOne,
+      numberOne: Number(numberOne)
+    },
+    {
+      playerTwo: playerTwo,
+      numberTwo: Number(numberTwo),
+    },
+    {
+      playerThree: playerThree,
+      numberThree: Number(numberThree)
+    }]
+
+  axios({
+    method: 'POST',
+    url: '/round',
+    data: newRound
+  })
+  // .then((response) => {
+  //   getRound()
+  // })
+  
+}
+
+function resetRound() {
+  axios({
+    method: 'POST',
+    url: '/randomNumber',
+    data: randomNumber
+  }).then((response) => {
+    getRound()
+  })
+}
+
+
+
+
+// function submitRandom(event) {
+//   event.preventDefault()
+//   let playerOne = document.getElementById('playerOne').value;
+//   let playerTwo = document.getElementById('playerTwo').value;
+//   let playerThree = document.getElementById('playerThree').value;
+//   let numberOne = document.getElementById('oneNumber').value;
+//   let numberTwo = document.getElementById('twoNumber').value;
+//   let numberThree = document.getElementById('threeNumber').value;
+//   document.getElementById('playerOne').value = '';
+//   document.getElementById('playerTwo').value = '';
+//   document.getElementById('playerThree').value = '';
+//   document.getElementById('oneNumber').value = '';
+//   document.getElementById('twoNumber').value = '';
+//   document.getElementById('threeNumber').value = '';
+//   let newRound = {
+//     playerOne: playerOne,
+//     playerTwo: playerTwo,
+//     playerThree: playerThree,
+//     numberOne: Number(numberOne),
+//     numberTwo: Number(numberTwo),
+//     numberThree: Number(numberThree),
+//   }
+//   // let newRound = {
+//   //   playerOne: playerOne,
+//   //   playerTwo: playerTwo,
+//   //   playerThree: playerThree,
+//   //   numberOne: Number(numberOne),
+//   //   numberTwo: Number(numberTwo),
+//   //   numberThree: Number(numberThree),
+//   // }
+//   axios({
+//     method: 'POST',
+//     url: '/round',
+//     data: newRound
+// }).then((response) => {
+//     getRound()
+// })
+//   console.log("JavaScript is loaded!")
+// }
+// function resetRound(){
+// axios({
+//     method: 'POST',
+//     url: '/randomNumber',
+//     data: randomNumber
+// }).then((response) => {
+//     getRound()
+// })
+// }
+// function getRound() {
+//   axios({
+//     url: '/round',
+//     method: 'GET'
+// }).then((response) => {
+//     console.log('response.data:', response.data)
+//     let currentRound = response.data.array
+//     let randomNumber = response.data.randomNumber
+//     //let contentDiv = document.querySelector('');
+//     renderRound(currentRound, randomNumber)
+// })
+// }
+// function renderRound(currentRound, randomNumber) {
+// for (let round of rounds) {
+//   if (round.numberOne === randomNumber) {
+//     renderWinnerOne(currentRound)
+//   }
+//   else if (round.numberTwo === randomNumber) {
+//     renderWinnerTwo()
+//   }
+//   else if (round.numberThree === randomNumber) {
+//     renderWinnerThree()
+//   }
+//   else {renderLoser()}
+// }
+// }
+// function renderWinnerOne(currentRound){
+//   let rounds = document.getElementById('playerRound');
+//   rounds.innerHTML = '';
+//   rounds.innerHTML += `
+//   <td class = "Winner">${currentRound.numberOne}</td>
+//   <td>${currentRound.numberTwo}</td>
+//   <td>${currentRound.numberThree}</td>
+// `}
+// function renderWinnerTwo(currentRound){
+//   let rounds = document.getElementById('playerRound');
+//   rounds.innerHTML = '';
+//   rounds.innerHTML += `
+//   <td>${currentRound.numberOne}</td>
+//   <td class = "Winner">${currentRound.numberTwo}</td>
+//   <td>${currentRound.numberThree}</td>
+// `}
+// function renderWinnerThree(currentRound){
+//   let rounds = document.getElementById('playerRound');
+//   rounds.innerHTML = '';
+//   rounds.innerHTML += `
+//   <td>${currentRound.numberOne}</td>
+//   <td>${currentRound.numberTwo}</td>
+//   <td class = "Winner">${currentRound.numberThree}</td>
+// `}
+// function renderLoser(currentRound){
+//   let rounds = document.getElementById('playerRound');
+//   rounds.innerHTML = '';
+//   rounds.innerHTML += `
+//   <td>${currentRound.numberOne}</td>
+//   <td>${currentRound.numberTwo}</td>
+//   <td>${currentRound.numberThree}</td>
+// `}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
+
+let randomNumber;
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -10,6 +12,25 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+app.post('/randomNumber', (req, res) => {
+  console.log('POST /randomNumber is getting requeset')
+  console.log('req.body', req.body)
+  generateRandomNumber()
+  res.sendStatus(201)
+})
+
+function generateRandomNumber() {
+  randomNumber = Math.floor((Math.random()* 25 + 1));
+  console.log(randomNumber);
+ };
+
+ app.post('/round', (req, res) => {
+  console.log('POST /round is getting requeset')
+  console.log('req.body', req.body)
+  
+  res.sendStatus(201)
+})
+ 
 
 
 app.listen(PORT, () => {
